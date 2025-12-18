3384e13dcfbfba632c66d3abc1cb54682e53bbfa
Theo <124263428+home-gihub@users.noreply.github.com>
2025-12-18 16:39:54 +0000

    overhaul

---
+++ creations/springfieldpolicedepartment/script.js
@@ -0,0 +1,29 @@
+var video = document.querySelector('video#video');
+var videosrc = document.querySelector('source');
+var promise = video.play();
+
+if (promise !== undefined) {
+  promise.then(_ => {
+    var yes = document.querySelector("input#yes")
+yes.addEventListener('click',  function(){
+    window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+})
+
+    var no = document.querySelector("input#no")
+no.addEventListener('click',  function(){
+    videosrc.src = './clickno.mp4'
+    video.load()
+    video.play()
+    document.querySelector('form').remove()
+    setTimeout(() => {
+        videosrc.src = './whilewait.mp4'
+    video.load()
+    video.play()
+    }, 7100);
+})
+
+
+  }).catch(error => {
+    document.body.innerHTML = '<h1>Please enable autoplay!</h1>'
+  });
+} 
