d84ff87cbfb4dc41c6f2eb2f0dac0ccd9e2787f0
Theo <124263428+home-gihub@users.noreply.github.com>
2026-03-06 11:15:44 +0000

    sub pages(hopefully) and a half-baked game launcher

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 function hideallsections(except){
-    for(i=1;i<5;i++){
-        if(i!=except) {
                 document.getElementById('s1').className='section'
     document.getElementById('s2').className='section'
     document.getElementById('s3').className='section'
     document.getElementById('s4').className='section'
-        }
+     document.getElementById('bladeSwitch' + String(except)).currentTime = 0;
+    document.getElementById('bladeSwitch' + String(except)).play()
 
-    }
+    
 
 }
 
@@ -26,26 +25,54 @@ function alignallblades(currentpage){
 }
 
 
+function shrinkallblades(){
+    for(i=1;i<5;i++){
+  document.getElementById('blade' + String(i)).className+=` shrinking`
+
+    }
+
+}
+
+
 document.getElementById('blade1').addEventListener('click', function(){
     hideallsections(1)
     document.getElementById('s1').className='section open'
     alignallblades(1)
+    setTimeout(function(){
+        document.getElementById('bladeLand').play()
+    },250)
 })
 
 document.getElementById('blade2').addEventListener('click', function(){
     hideallsections(2)
     document.getElementById('s2').className='section open'
      alignallblades(2)
+     setTimeout(function(){
+        document.getElementById('bladeLand').play()
+    },250)
 })
 
 document.getElementById('blade3').addEventListener('click', function(){
     hideallsections(3)
     document.getElementById('s3').className='section open'
      alignallblades(3)
+     setTimeout(function(){
+        document.getElementById('bladeLand').play()
+    },250)
 })
 
 document.getElementById('blade4').addEventListener('click', function(){
     hideallsections(4)
     document.getElementById('s4').className='section open'
      alignallblades(4)
+     setTimeout(function(){
+        document.getElementById('bladeLand').play()
+    },250)
 })
+
+function visitsub(link,sec) {
+    shrinkallblades()
+    document.getElementById('enterSub').play()
+    document.getElementById('s'+String(sec)).className="full section"
+    setTimeout(function(){window.location = link}, 1010)   
+}
