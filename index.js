0631f10cfe44d9f9fe53ae7bcdf26ca6b8f842f4
Theo <124263428+home-gihub@users.noreply.github.com>
2026-05-08 16:59:22 +0100

    theme changer

--- index.js
+++ index.js
@@ -2,6 +2,43 @@ function gowanim(url) {
     document.getElementById("pagecontainer").removeAttribute("goin")
     document.getElementById("pagecontainer").setAttribute("goout", "")
     setTimeout(() => {
-        window.location=url
+        fetch(url)
+  .then(response => {
+    // When the page is loaded convert it to text
+    return response.text()
+  })
+  .then(html => {
+    // Initialize the DOM parser
+    const parser = new DOMParser()
+
+    // Parse the text
+    const doc = parser.parseFromString(html, "text/html")
+       document.getElementsByTagName("title")[0].innerText = doc.getElementsByTagName("title")[0].innerText
+ 
+    document.body=doc.body
+    // You can now even select part of that html as you would in the regular DOM
+    // Example:
+    // const docArticle = doc.querySelector('article').innerHTML
+
+    console.log(doc)
+  })
+  .catch(error => {
+     console.error('Failed to fetch page: ', error)
+  })
+       // window.location=url
     }, 1500);
+}
+cookies = {"theme":""}
+if (document.cookie==null || document.cookie == "null") {
+   settheme("purple.css")
+} else {
+ cookies = JSON.parse(document.cookie)
+}
+
+document.head.innerHTML +=`<link rel="stylesheet" href="${cookies["theme"]}"/>` 
+
+function settheme(theme) {
+    
+   cookies["theme"] = theme
+    document.cookie = JSON.stringify(cookies)
 }
